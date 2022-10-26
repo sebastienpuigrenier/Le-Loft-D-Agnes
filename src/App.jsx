0e615a6c5dd22394cdf3998e59c8c83d522ce5d1
Sebastien Puigrenier <sebastien.puigrenier@gmail.com>
2022-10-26 22:58:16 +0200

    mise en place du footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { Footer } from "./components/organismes/footer/footer";
 
 import Index from "./components/pages/Index";
+
+import './App.css';
+import './base.css';
+
 
 function App() {
   
@@ -12,6 +17,7 @@ function App() {
           <Route path="/*" element={<Index />} />
         </Routes>
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/organismes/footer/footer.jsx
@@ -0,0 +1,24 @@
+import { AiFillMail, AiFillFacebook, AiFillInstagram } from "react-icons/ai";
+import "./footer.css";
+import svgLogo from "../../../img/le-loft-d-agnes-logo.svg";
+
+export const Footer =() => {
+
+  return(
+    <footer>
+      <div>
+        <img id="svglogo" src={svgLogo} alt="Le loft d'Agnès"/>
+      </div>
+      <div>
+        <p>Création de planche apéro, meubles et autres objets déco personnalisées.</p>
+      </div>
+      <div id="social-media">
+        <ul>
+          <li><a href="mailto: agnes.guichet@sfr.fr"><AiFillMail /></a></li>
+          <li><a href="https://www.facebook.com/leloftdagnes"><AiFillFacebook /></a></li>
+          <li><a href="https://www.instagram.com/leloftdagnes/?hl=fr"><AiFillInstagram /></a></li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
